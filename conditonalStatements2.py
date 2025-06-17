5e019c74742d18c75a652645223e63a47e76ed82
Khrithik <khrithikjupalli@gmail.com>
2025-06-17 10:48:28 +0530

    Added 2nd Question

---
+++ conditonalStatements2.py
@@ -0,0 +1,14 @@
+# conditonalStatements
+
+# Check if a number is positive, negative, or zero
+
+num = int(input("Enter a number : "))
+
+if num>0:
+    print("positive")
+
+elif num == 0:
+    print("zero")
+
+else:
+    print("negative")
